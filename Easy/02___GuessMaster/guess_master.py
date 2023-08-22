4a7434006ed0a778c44cf2e49cd2f26e8642fac8
Prathik Vishavadiya <108474251+CuriousPrathik@users.noreply.github.com>
2023-08-22 22:47:34 +0530

    added fun

--- Easy/02___GuessMaster/guess_master.py
+++ Easy/02___GuessMaster/guess_master.py
@@ -21,5 +21,33 @@ def choose_difficulty():
         print("Invalid choice. Defaulting to Medium.")
         return 1, 100, 7
 
+def play_game():
+    secret_number = random.randint(start_range, end_range)
+    attempts_left = max_attempts
+
+    while attempts_left > 0:
+        guess = int(input("\nEnter your guess: "))
+        
+        if guess < secret_number:
+            print("Too low!")
+        elif guess > secret_number:
+            print("Too high!")
+        else:
+            print(f"Congratulations! You've guessed the secret number {secret_number}!")
+            print(f"You used {max_attempts - attempts_left + 1} attempts.")
+            break
+        
+        attempts_left -= 1
+        print(f"Attempts left: {attempts_left}")
+
+    if attempts_left == 0:
+        print(f"\nSorry, you've run out of attempts. The secret number was {secret_number}.")
+
+def main():
+    display_welcome()
+    global start_range, end_range, max_attempts
+    start_range, end_range, max_attempts = choose_difficulty()
+    play_game()
+
 if __name__ == "__main__":
-    pass
+    main()
